a28ab8ca378a105ea37172d7ba3922cc04a20dd8
Iconmaster <crazybonesj@yahoo.com>
2015-01-26 19:05:52 -0500

    More math operators

--- src/com/iconmaster/sbcore/library/CoreFunctions.java
+++ src/com/iconmaster/sbcore/library/CoreFunctions.java
@@ -53,6 +53,131 @@ public class CoreFunctions {
 			return new SourceObject(args[0].type, ((float)args[0].data)+((float)args[1].data));
 		});
 		
+		pkg.getFunction("core.int._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)-((int)args[1].data));
+		});
+		pkg.getFunction("core.int64._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((long)args[0].data)-((long)args[1].data));
+		});
+		pkg.getFunction("core.int32._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)-((int)args[1].data));
+		});
+		pkg.getFunction("core.int16._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((short)args[0].data)-((short)args[1].data));
+		});
+		pkg.getFunction("core.int8._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((byte)args[0].data)-((byte)args[1].data));
+		});
+		pkg.getFunction("core.real._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)-((float)args[1].data));
+		});
+		pkg.getFunction("core.real64._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((double)args[0].data)-((double)args[1].data));
+		});
+		pkg.getFunction("core.real32._sub").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)-((float)args[1].data));
+		});
+		
+		pkg.getFunction("core.int._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)*((int)args[1].data));
+		});
+		pkg.getFunction("core.int64._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((long)args[0].data)*((long)args[1].data));
+		});
+		pkg.getFunction("core.int32._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)*((int)args[1].data));
+		});
+		pkg.getFunction("core.int16._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((short)args[0].data)*((short)args[1].data));
+		});
+		pkg.getFunction("core.int8._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((byte)args[0].data)*((byte)args[1].data));
+		});
+		pkg.getFunction("core.real._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)*((float)args[1].data));
+		});
+		pkg.getFunction("core.real64._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((double)args[0].data)*((double)args[1].data));
+		});
+		pkg.getFunction("core.real32._mul").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)*((float)args[1].data));
+		});
+		
+		pkg.getFunction("core.int._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)/((int)args[1].data));
+		});
+		pkg.getFunction("core.int64._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((long)args[0].data)/((long)args[1].data));
+		});
+		pkg.getFunction("core.int32._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)/((int)args[1].data));
+		});
+		pkg.getFunction("core.int16._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((short)args[0].data)/((short)args[1].data));
+		});
+		pkg.getFunction("core.int8._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((byte)args[0].data)/((byte)args[1].data));
+		});
+		pkg.getFunction("core.real._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)/((float)args[1].data));
+		});
+		pkg.getFunction("core.real64._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((double)args[0].data)/((double)args[1].data));
+		});
+		pkg.getFunction("core.real32._div").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)/((float)args[1].data));
+		});
+		
+		pkg.getFunction("core.int._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)%((int)args[1].data));
+		});
+		pkg.getFunction("core.int64._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((long)args[0].data)%((long)args[1].data));
+		});
+		pkg.getFunction("core.int32._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((int)args[0].data)%((int)args[1].data));
+		});
+		pkg.getFunction("core.int16._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((short)args[0].data)%((short)args[1].data));
+		});
+		pkg.getFunction("core.int8._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((byte)args[0].data)%((byte)args[1].data));
+		});
+		pkg.getFunction("core.real._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)%((float)args[1].data));
+		});
+		pkg.getFunction("core.real64._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((double)args[0].data)%((double)args[1].data));
+		});
+		pkg.getFunction("core.real32._mod").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, ((float)args[0].data)%((float)args[1].data));
+		});
+		
+		pkg.getFunction("core.int._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (int) Math.pow(((int)args[0].data),((int)args[1].data)));
+		});
+		pkg.getFunction("core.int64._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (long) Math.pow(((long)args[0].data),((long)args[1].data)));
+		});
+		pkg.getFunction("core.int32._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (int) Math.pow(((int)args[0].data),((int)args[1].data)));
+		});
+		pkg.getFunction("core.int16._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (short) Math.pow((short)args[0].data,((short)args[1].data)));
+		});
+		pkg.getFunction("core.int8._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (byte) Math.pow(((byte)args[0].data),((byte)args[1].data)));
+		});
+		pkg.getFunction("core.real._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (float) Math.pow(((float)args[0].data),((float)args[1].data)));
+		});
+		pkg.getFunction("core.real64._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, Math.pow(((double)args[0].data),((double)args[1].data)));
+		});
+		pkg.getFunction("core.real32._pow").data.put("onRun",(CustomFunction) (vm, args) -> {
+			return new SourceObject(args[0].type, (float) Math.pow(((float)args[0].data),((float)args[1].data)));
+		});
+		
 		pkg.getIterator("core.range").data.put("onRun",(CustomIterator) (vm, args) -> {
 			ArrayList<SourceObject[]> pairs = new ArrayList<>();
 			int begin = ((Number)args[0].data).intValue();
